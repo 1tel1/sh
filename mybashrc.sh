2e3b5343159722094ca8f72734b1f3486cb2b1cb
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-08-19 00:49:52 +0800

    Update mybashrc.sh

--- mybashrc.sh
+++ mybashrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d "~/.bashrc1" ]; then
   mv ~/.bashrc ~/.bashrc1
-  wget https://raw.githubusercontent.com/1tel1/sh/main/.bashrc
+  wget https://fastly.jsdelivr.net/gh/1tel1/sh@main/.bashrc ~/.bashrc
   source ~/.bashrc
 fi
