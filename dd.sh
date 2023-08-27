6fedf771b3991cf0d559e06649aafa7b28e6753d
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-08-28 03:21:24 +0800

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -3,8 +3,8 @@ apt-get install -y xz-utils openssl gawk file wget screen && screen -S os || yum
 ufw status
 if [ $? -ne 0 ]; then
     apt update -y && apt dist-upgrade -y || yum makecache && yum update -y
-else
-    wget --no-check-certificate -O AutoReinstall.sh https://d.02es.com/AutoReinstall.sh && chmod a+x AutoReinstall.sh && bash AutoReinstall.sh
 fi
+    wget --no-check-certificate -O AutoReinstall.sh https://d.02es.com/AutoReinstall.sh && chmod a+x AutoReinstall.sh && bash AutoReinstall.sh
+
 
 
