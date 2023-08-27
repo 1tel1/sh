5eddd5a974fc18a17cdc72ff86c16510cfbd299c
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-08-27 23:44:59 +0800

    Update docker-de.sh

--- docker-de.sh
+++ docker-de.sh
@@ -12,6 +12,7 @@ apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 else
+  apt-get remove docker docker-engine docker.io containerd runc
    sudo apt-get update  &&  sudo apt-get upgrade
    sudo apt-get install \
 	apt-transport-https \
@@ -41,3 +42,4 @@ echo "alias d-ps='docker ps' " >> ~/.bashrc
 echo "alias d-cu='docker-compose up -d' " >> ~/.bashrc
 echo "alias d-do='docker-compose down' " >> ~/.bashrc
 source ~/.bashrc
+rm -rf docker-de.sh
