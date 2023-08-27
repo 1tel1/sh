29491dfb60fe47da28b21f3ac8c603656992500c
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-08-27 13:36:01 +0800

    Update docker-de.sh

--- docker-de.sh
+++ docker-de.sh
@@ -8,9 +8,9 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-curl -L https://get.daocloud.io/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 echo "alias d-st='docker stop' " >> ~/.bashrc
 echo "alias d-rm='docker rm' " >> ~/.bashrc
 echo "alias d-im='docker images' " >> ~/.bashrc
