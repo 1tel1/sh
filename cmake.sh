96c67c9c184f3fb279f3b3347e1cc9b8a4faf5a2
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-08-27 19:53:15 +0800

    Create cmake.sh

---
+++ cmake.sh
@@ -0,0 +1,7 @@
+wget  https://gh.con.sh/https://github.com/Kitware/CMake/releases/download/v3.27.4/cmake-3.27.4.tar.gz
+tar -zxvf cmake-3.27.4.tar.gz
+cd cmake-3.27.4 && rm -rf cmake-3.27.4.tar.gz
+./bootstrap --prefix=/usr/local
+make
+make install
+/usr/local/bin/cmake --version
