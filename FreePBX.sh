a36d24f42e4491ecbcd8c79d85fb528c1466a856
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-08-30 13:39:21 +0800

    Update FreePBX.sh

--- FreePBX.sh
+++ FreePBX.sh
@@ -3,14 +3,13 @@ my = ""
 echo "以sudo身份运行       sudo su -"
 sudo apt-get update && sudo apt-get upgrade
 echo "安装MongoDB"
-sudo apt update
 sudo apt dist-upgrade -y
 sudo apt install gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 sudo apt update
 sudo apt install -y mongodb-org
-sudo systemctl enable mongod
+# sudo systemctl enable mongod
 sudo service mongod start
 sudo service mongod status
 if [ $? -eq 0 ]; then
@@ -28,9 +27,9 @@ else
 	echo "开始安装MariaDB"
 fi
 sudo apt -y install mariadb-server mariadb-client
-# sudo systemctl start mariadb
-# sudo systemctl enable mariadb
-# sudo mysql_secure_installation 
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+sudo mysql_secure_installation 
 if [ $? -eq 0 ]; then
     echo "MariaDB 成功"
     my = " ${my}
