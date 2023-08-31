886ab7b62f68ee2ac4f1d9013a291a44d1038f23
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-08-31 12:39:19 +0800

    Update cmake.sh

--- cmake.sh
+++ cmake.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 wget  https://gh.con.sh/https://github.com/Kitware/CMake/releases/download/v3.27.4/cmake-3.27.4.tar.gz
 tar -zxvf cmake-3.27.4.tar.gz
 cd cmake-3.27.4 && rm -rf cmake-3.27.4.tar.gz
