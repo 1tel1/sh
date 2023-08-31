dc1c6bd1cfe7b351d70c726d8ea054c3ca620642
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-08-31 13:02:46 +0800

    Update cmake.sh

--- cmake.sh
+++ cmake.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+sudo apt install build-essential checkinstall zlib1g-dev libssl-dev
 wget  https://gh.con.sh/https://github.com/Kitware/CMake/releases/download/v3.27.4/cmake-3.27.4.tar.gz
 tar -zxvf cmake-3.27.4.tar.gz
 cd cmake-3.27.4 && rm -rf cmake-3.27.4.tar.gz
-./bootstrap --prefix=/usr/local
-make
-make install
+sudo ./bootstrap 
+sudo make
+sudo make install
 /usr/local/bin/cmake --version
