adcb3e0b0b895fd6fe46a7b2cd7d2f6baee69f40
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-09-02 08:27:23 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,21 +4,50 @@ sudo apt-get purge docker-ce docker-ce-cli containerd.io
 sudo rm -rf /var/lib/docker  
 sudo rm -rf /var/lib/containerd  
 
-read -p "国内服务器请输入1,国外2》》》》》》》:" word
+read -p "国内服务器请输入1,国外2,centos 3》》》》》》》:" word
 if [ $word = 2 ] ; then
 wget -qO- get.docker.com | bash  
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose  
 elif [ $word = 1 ] ; then
 curl -sSL https://get.daocloud.io/docker | sh  
 curl -L https://get.daocloud.io/docker/compose/releases/download/v2.1.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose  
+elif [ $word = 3 ] ; then
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
 fi
-sudo chmod +x /usr/local/bin/docker-compose  
+
+docker -v
+if [ $? -ne 0 ]; then
+  for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+else
+    sudo chmod +x /usr/local/bin/docker-compose  
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
   "registry-mirrors": ["https://6m1uvtgj.mirror.aliyuncs.com"]
 }
 EOF
+fi
+
 
 sudo systemctl daemon-reload
 sudo systemctl restart docker
