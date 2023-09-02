e58c034818692c33ad9c8b1f43458b0d9d894ea5
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-09-02 08:49:49 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,15 +1,20 @@
 #! /bin/bash  
 apt-get update && apt-get install -y wget vim  
-sudo apt-get purge docker-ce docker-ce-cli containerd.io  
-sudo rm -rf /var/lib/docker  
-sudo rm -rf /var/lib/containerd  
-
+sudo apt-get remove docker-*
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 read -p "国内服务器请输入1,国外2,centos 3》》》》》》》:" word
-if [ $word = 2 ] ; then
-wget -qO- get.docker.com | bash  
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose  
-elif [ $word = 1 ] ; then
-curl -sSL https://get.daocloud.io/docker | sh  
+if [ $word = 1 ] ; then
+curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://mirrors.aliyun.com/docker-ce/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo curl -L https://download.fastgit.org/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+elif [ $word = 2 ] ; then
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 elif [ $word = 3 ] ; then
 sudo yum remove docker \
                   docker-client \
@@ -22,7 +27,12 @@ sudo yum remove docker \
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker
 sudo systemctl start docker
+sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 docker -v
