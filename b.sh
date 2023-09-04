9bc5497dfb7af6f3bb42df7817d98d2e2a25f130
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-09-04 09:02:51 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,7 +1,6 @@
-
-
+#!/usr/bin/env bash
 sudo apt-get install python3
 pip3 install nats-py
-vim /usr/local/xswitch*/transfer.py
+wget https://ghproxy.com/https://raw.githubusercontent.com/1tel1/sh/main/t.py /usr/local/xswitch*/transfer.py
 
 
