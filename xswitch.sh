e63f47eb047750e6edb2e199ee768a878cbb0d37
1tel1 <142546877+1tel1@users.noreply.github.com>
2023-09-08 04:49:58 +0800

    Update xswitch.sh

--- xswitch.sh
+++ xswitch.sh
@@ -16,15 +16,48 @@ chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 fi
 cd /usr/local
-wget https://xswitch.cn/download/xswitch-6.1.7.9.tar.gz --user xswitch --password password
-tar zxvf xswitch-6.1.7.9.tar.gz -C /usr/local
-rm -rf xswitch-6.1.7.9.tar.gz
-mv /usr/local/xswitch-6.1.7.9 /usr/local/xswitch
+read -p "1:社区版，2:企业版" word1
+if [ $word1 == 1 ] ;then
+  read -p "1:稳定版，2:开发版" word2
+  if [ $word2 == 1 ] ;then
+    wget https://xswitch.cn/download/xswitch-community-6.0.7.tar.gz --user xswitch --password password
+    tar zxvf xswitch-community-6.0.7.tar.gz -C /usr/local
+    rm -rf xswitch-community-6.0.7.tar.gz
+    mv /usr/local/xswitch-community-6.0.7 /usr/local/xswitch
+  elif [ $word2 == 2 ] ;then
+    wget https://xswitch.cn/download/xswitch-community-6.1.7.9.tar.gz --user xswitch --password password
+    tar zxvf xswitch-community-6.1.7.9.tar.gz -C /usr/local
+    rm -rf xswitch-community-6.1.7.9.tar.gz
+    mv /usr/local/xswitch-community-6.1.7.9 /usr/local/xswitch
+  fi
+elif [ $word1 == 2 ] ;then  
+  read -p "1:稳定版，2:开发版" word3
+  if [ $word3 == 1 ] ;then
+    wget https://xswitch.cn/download/xswitch-6.0.7.tar.gz --user xswitch --password password
+    tar zxvf xswitch-6.0.7.tar.gz -C /usr/local
+    rm -rf xswitch-6.0.7.tar.gz
+    mv /usr/local/xswitch-6.0.7 /usr/local/xswitch
+  elif [ $word3 == 2 ] ;then
+    wget https://xswitch.cn/download/xswitch-6.1.7.9.tar.gz --user xswitch --password password
+    tar zxvf xswitch-6.1.7.9.tar.gz -C /usr/local
+    rm -rf xswitch-6.1.7.9.tar.gz
+    mv /usr/local/xswitch-6.1.7.9 /usr/local/xswitch
+  fi
+fi
 cd /usr/local/xswitch
 make setup
 ip1= curl ifconfig.me
+read -p "公网IP：$ip1 ，如不正确请重新输入：" ip11
+if [ ${#ip11} -gt 2 ] ; then
+	ip1 = ${ip11} 
+fi
+
 networkCard=`ifconfig | grep RUNNING |grep BROADCAST| awk -F ':' '{print $1}'`
 ip=`ifconfig "$networkCard"|grep inet|grep -v inet6|awk '{print $2}'`
+read -p "内网IP：$ip ，如不正确请重新输入：" ip12
+if [ ${#ip12} -gt 2 ] ; then
+	ip = ${ip12} 
+fi
 sed 's/EXT_IP=2.2.2.2/EXT_IP=$ip1/g' /usr/local/xswitch/.env
 sed 's/LOCAL_IP=192.168.0.1/LOCAL_IP=$ip/g' /usr/local/xswitch/.env
 sed 's/NGINX_PROXY=192.168.0.1/NGINX_PROXY=$ip/g' /usr/local/xswitch/.env
